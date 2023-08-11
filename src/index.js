e6b662e43003fe58ed2bd3bdd3ad1700dcb2a598
Zeeshan <zeeshansheray1@gmail.com>
2023-08-12 02:46:52 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import '../src/assets/css/global.scss'
 
-export function embedApp(containerId) {
-  console.log('containerId ',containerId)
-  ReactDOM.render(<App />, document.getElementById(containerId || 'root'));
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
