8893c02b9ea5c7617ac622e923387d3f86020c74
Zeeshan <zeeshansheray1@gmail.com>
2023-08-12 02:20:21 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '../src/assets/css/global.scss'
 
-export default function embedApp(containerId) {
+export function embedApp(containerId) {
   ReactDOM.render(<App />, document.getElementById(containerId || 'root'));
 }
 
