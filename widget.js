5bfc4f6e052a8a5d2f1dbaa03f2b843886360e80
Zeeshan <zeeshansheray1@gmail.com>
2023-08-12 01:45:42 +0500

    changes

---
+++ widget.js
@@ -0,0 +1,31 @@
+// widget.js
+
+// Define a function to initialize the CounterWidget
+function initCounterWidget(containerId) {
+    const container = document.getElementById(containerId) || document.createElement('div');
+  
+    if (!document.getElementById(containerId)) {
+      document.body.appendChild(container);
+    }
+  
+    // Load the widget's built JavaScript and CSS files
+    const script = document.createElement('script');
+    script.src = './build/static/js/main.eaafacdb.js'; // Replace with your actual path
+    document.head.appendChild(script);
+  
+    // Wait for the script to load
+    script.onload = () => {
+      // At this point, the CounterWidget initialization function should be available
+      initWidgetComponent(container); // Call the function to initialize the widget
+    };
+  }
+  
+  // Placeholder function for initializing the CounterWidget component
+  function initWidgetComponent(container) {
+    // This function should be defined in your main.eaafacdb.js file
+    // It initializes the CounterWidget and renders it in the specified container
+    // For example: initCounterWidget(container);
+  }
+  
+  window.initCounterWidget = initCounterWidget;
+  
