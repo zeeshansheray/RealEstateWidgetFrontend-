e6c5834a5b4228702f17bb5ab7542056df126c8f
Zeeshan <zeeshansheray1@gmail.com>
2023-08-12 02:23:03 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import '../src/assets/css/global.scss'
 
 export function embedApp(containerId) {
+  console.log('containerId ',containerId)
   ReactDOM.render(<App />, document.getElementById(containerId || 'root'));
 }
 
